5856b4e74fd64551718d4bc8ad91256ad06f929a
Raihan Kibria <rkibria@users.noreply.github.com>
2018-12-27 18:19:15 +0100

    pylint fixes

--- dct.py
+++ dct.py
@@ -10,87 +10,95 @@ import math
 import unittest
 import random
 
-def get_dct(x):
-    """Apply DCT on list of numbers x, return list with same number of elements"""
-    NN = len(x)
-    r = [0.0] * NN
-    if NN > 0:
-        for k in range(NN):
-            for n in range(NN):
-                r[k] += x[n] * math.cos(math.pi / NN * k * (n + 0.5))
-        r[0] *= 1.0 / math.sqrt(2.0)
-        for n in range(NN):
-            r[n] *= math.sqrt(2.0 / NN)
-    return r
+def get_dct(input_values):
+    """
+    Apply DCT on list of numbers input_values,
+    return list with same number of elements
+    """
+    matrix_size = len(input_values)
+    result = [0.0] * matrix_size
+    if matrix_size > 0:
+        for outer_index in range(matrix_size):
+            for index in range(matrix_size):
+                result[outer_index] += (input_values[index]
+                                        * math.cos(math.pi / matrix_size
+                                                   * outer_index * (index + 0.5)))
+        result[0] *= 1.0 / math.sqrt(2.0)
+        for index in range(matrix_size):
+            result[index] *= math.sqrt(2.0 / matrix_size)
+    return result
 
-def get_idct(x):
-    """Inverse DCT on list of numbers x"""
-    NN = len(x)
-    r = [0.0] * NN
-    for k in range(NN):
-        r[k] = x[0] / math.sqrt(2.0)
-        for n in range(1, NN):
-            r[k] += x[n] * math.cos(math.pi / NN * n * (k + 0.5))
-    for n in range(NN):
-        r[n] *= math.sqrt(2.0 / NN)
-    return r
+def get_idct(input_values):
+    """Inverse DCT on list of numbers input_values"""
+    matrix_size = len(input_values)
+    result = [0.0] * matrix_size
+    for outer_index in range(matrix_size):
+        result[outer_index] = input_values[0] / math.sqrt(2.0)
+        for index in range(1, matrix_size):
+            result[outer_index] += (input_values[index]
+                                    * math.cos(math.pi / matrix_size
+                                               * index * (outer_index + 0.5)))
+    for index in range(matrix_size):
+        result[index] *= math.sqrt(2.0 / matrix_size)
+    return result
 
-def get_2d_dct(m):
-    """Apply DCT on 2D matrix (nested list) of numbers m, return same size matrix"""
+def get_2d_dct(input_matrix):
+    """Apply DCT on 2D matrix (nested list) of numbers input_matrix, return same size matrix"""
 
-    NN = len(m)
-
-    def c_factor(i):
-        if i == 0:
-            return 1.0 / math.sqrt(2.0)
-        else:
-            return 1.0
-
-    def cos_term(inner, outer):
-        return math.cos(math.pi * outer * (2.0 * inner + 1.0) / (2.0 * NN))
-
-    if NN > 0:
-        r = []
-        for i in range(NN):
-            r.append([0.0] * NN)
-
-        for i in range(NN):
-            for j in range(NN):
-                for x in range(NN):
-                    for y in range(NN):
-                        r[i][j] += m[x][y] * cos_term(x, i) * cos_term(y, j)
-                r[i][j] *= c_factor(i) * c_factor(j) * 2.0 / NN
-
-    return r
-
-def get_2d_idct(m):
-    """Apply Inverse DCT on 2D matrix (nested list) of numbers m, return same size matrix"""
-
-    NN = len(m)
+    matrix_size = len(input_matrix)
 
     def c_factor(i):
         if i == 0:
             return 1.0 / math.sqrt(2.0)
-        else:
-            return 1.0
+        return 1.0
 
     def cos_term(inner, outer):
-        return math.cos(math.pi * inner * (2.0 * outer + 1.0) / (2.0 * NN))
+        return math.cos(math.pi * outer * (2.0 * inner + 1.0) / (2.0 * matrix_size))
 
-    if NN > 0:
-        r = []
-        for i in range(NN):
-            r.append([0.0] * NN)
+    if matrix_size > 0:
+        result = []
+        for i in range(matrix_size):
+            result.append([0.0] * matrix_size)
 
-        for i in range(NN):
-            for j in range(NN):
-                for x in range(NN):
-                    for y in range(NN):
-                        r[i][j] += (c_factor(x) * c_factor(y) * m[x][y]
-                                    * cos_term(x, i) * cos_term(y, j))
-                r[i][j] *= 2.0 / NN
+        for i in range(matrix_size):
+            for j in range(matrix_size):
+                for x in range(matrix_size):
+                    for y in range(matrix_size):
+                        result[i][j] += input_matrix[x][y] * cos_term(x, i) * cos_term(y, j)
+                result[i][j] *= c_factor(i) * c_factor(j) * 2.0 / matrix_size
 
-    return r
+    return result
+
+def get_2d_idct(input_matrix):
+    """
+    Apply Inverse DCT on 2D matrix (nested list) of
+    numbers input_matrix, return same size matrix
+    """
+
+    matrix_size = len(input_matrix)
+
+    def c_factor(i):
+        if i == 0:
+            return 1.0 / math.sqrt(2.0)
+        return 1.0
+
+    def cos_term(inner, outer):
+        return math.cos(math.pi * inner * (2.0 * outer + 1.0) / (2.0 * matrix_size))
+
+    if matrix_size > 0:
+        result = []
+        for i in range(matrix_size):
+            result.append([0.0] * matrix_size)
+
+        for i in range(matrix_size):
+            for j in range(matrix_size):
+                for x in range(matrix_size):
+                    for y in range(matrix_size):
+                        result[i][j] += (c_factor(x) * c_factor(y) * input_matrix[x][y]
+                                         * cos_term(x, i) * cos_term(y, j))
+                result[i][j] *= 2.0 / matrix_size
+
+    return result
 
 class TestDCT(unittest.TestCase):
     def test_1d(self):
